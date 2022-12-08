22c0e06c05662679a8b7758e47cbdde96903c1b9
Nitinyad <92179118+Nitinyad@users.noreply.github.com>
2022-12-08 21:47:03 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,17 @@
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBaYIGtqrK4tdfw1VDpxK5T-jQOZ4Aezik",
+    authDomain: "login-page-5a5fb.firebaseapp.com",
+    databaseURL: "https://login-page-5a5fb-default-rtdb.firebaseio.com",
+    projectId: "login-page-5a5fb",
+    storageBucket: "login-page-5a5fb.appspot.com",
+    messagingSenderId: "98597240350",
+    appId: "1:98597240350:web:2f27fdd1899118a13c380f"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
+
+
